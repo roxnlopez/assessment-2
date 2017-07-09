05c3264b1a1a1eea96f91dc017775a6f33e14801
roxnlopez <roxnlopez@gmail.com>
2017-07-09 13:40:30 -0600

    stuff

--- main.js
+++ main.js
@@ -1,4 +1,39 @@
-blue.addEventListener("click", function(){
+
+var player1 = document.createElement('.blue');
+
+function player1() {
+  this.x = 0;
+  this.y = 0;
+}
+
+player1.prototype.draw = function(context) {
+  context.fillRect(this.x, this.y, 32, 32);
+};
+
+player1.prototype.moveLeft = function() {
+  this.x -= 1;
+};
+
+player1.prototype.moveRight = function() {
+  this.x += 1;
+};
+
+player1.prototype.moveUp = function() {
+  this.y -= 1;
+};
+
+player1.prototype.moveRight = function() {
+  this.y += 1;
+};
+
+
+game.start = function () {
+	game.player = new Player();	
+};
+
+
+
+player1.addEventListener("click", function(){
 	$(this).keydown();
 });
 
