123049e57e7dbe2e545bc3967680d429f6628ae3
roxnlopez <roxnlopez@gmail.com>
2017-07-10 20:44:09 -0600

    js comments

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 console.log("this is running");
+//up and running!
 
+//make some beginner var and globals
 function Player(car, margin) {
   this.car = car;
   this.marginTop = margin;
@@ -13,17 +15,20 @@ function Player(car, margin) {
   };
 }
 
+//need a play function with racers and the street
 function Game() {
   var track = $('.racetrack');
   var player1 = new Player($('#p1'), "10%");
   var player2 = new Player($('#p2'), "10%");
 
+//call listener function
   this.start = function() {
     player1.prepare();
     player2.prepare();
     setListener();
   };
 
+//make listener function that uses keyup/down/allaround (look up key numbers)
   var setListener = function() {
     $(document).keyup(function(event) {
       if (event.which == 38) {
@@ -31,7 +36,7 @@ function Game() {
       } else if (event.which == 40) {
         player2.move();
       }
-
+//do conditional that shows if or how racer wins...don't lose your mind.
       if (parseInt($(player1.car).css("margin-left")) >= parseInt($(track).css("width"))) {
         gameOver("player1");
       } else if (parseInt($(player2.car).css("margin-left")) >= parseInt($(track).css("width"))) {
@@ -41,6 +46,7 @@ function Game() {
 };
 }
 
+//make function to show winner and game is over...try to create reset button
 var gameOver = function(winner) {
 if (confirm("Game Over! " + winner + " wins! Hit ok to play again.")) {
     $(player1.car).css("margin-left", "0%");
